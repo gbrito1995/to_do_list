d3957557d2bb28bf4739d1ca8ca897dcba991a1c
Gabriel da Silva Brito <gabrieldsb95@gmail.com>
2022-06-15 12:57:03 -0300

    commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,9 @@
 let inputField = document.getElementById('taskInput');
 let addButton = document.getElementById('addButton');
 let itemList = document.getElementById('itemList').children[0];
+let taskAdded = document.getElementById('taskAdded');
 let list = [
-  /*{ "id": 99, "task": "Estudar", "isFinished": false },
+  /*{ "id": 99, "task": "Estudar", "isFinished": false },pop up notifications models
   { "id": 100, "task": "Treinar", "isFinished": false },
   { "id": 101, "task": "Jogar", "isFinished": false },
   { "id": 102, "task": "Trabalhar", "isFinished": false }*/
@@ -10,6 +11,9 @@ let list = [
 let i = 0;
 
 let fnAddTask = (task) => {
+
+  taskAdded.style.right = '3em';
+  taskAdded.style.transition = 'right 0.5s ease';
 
   if (task) {
     list.push({ id: ++i, task: task, isFinished: false });
