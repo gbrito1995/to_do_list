6ea2db891f01be7a1206cc686529bc5e8dac7c16
Gabriel da Silva Brito <gabrieldsb95@gmail.com>
2022-06-15 16:49:47 -0300

    commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,23 +2,26 @@ let inputField = document.getElementById('taskInput');
 let addButton = document.getElementById('addButton');
 let itemList = document.getElementById('itemList').children[0];
 let taskAdded = document.getElementById('taskAdded');
+let taskRemoved = document.getElementById('taskRemoved');
+let closeAdd = taskAdded.children[1];
+let closeRm = taskRemoved.children[1];
 let list = [
-  /*{ "id": 99, "task": "Estudar", "isFinished": false },pop up notifications models
+  { "id": 99, "task": "Estudar", "isFinished": false },
   { "id": 100, "task": "Treinar", "isFinished": false },
   { "id": 101, "task": "Jogar", "isFinished": false },
-  { "id": 102, "task": "Trabalhar", "isFinished": false }*/
+  { "id": 102, "task": "Trabalhar", "isFinished": false }
 ];
 let i = 0;
 
 let fnAddTask = (task) => {
 
-  taskAdded.style.right = '3em';
-  taskAdded.style.transition = 'right 0.5s ease';
-
   if (task) {
     list.push({ id: ++i, task: task, isFinished: false });
 
     fnAddItems(i, task);
+
+    taskAdded.style.right = '3em';
+    taskAdded.style.transition = 'right 2s';
   }
 
 }
@@ -57,6 +60,9 @@ let fnRmItems = (thisListId) => {
   list.splice(rmElementIndex, 1);
   rmElement.remove();
 
+  taskRemoved.style.right = '3em';
+  taskRemoved.style.transition = 'right 2s';
+
 }
 
 addButton.addEventListener('click', () => {
@@ -64,6 +70,21 @@ addButton.addEventListener('click', () => {
   fnAddTask(inputField.value);
 
 })
+
+closeAdd.addEventListener('click', () => {
+
+  taskAdded.style.right = '-20em';
+  taskAdded.style.transition = 'right 2s';
+
+})
+
+closeRm.addEventListener('click', () => {
+
+  taskRemoved.style.right = '-20em';
+  taskRemoved.style.transition = 'right 2s';
+
+})
+
 
 //teste
 list.forEach((item) => {
